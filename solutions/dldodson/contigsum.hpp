8c15dee5dc0cc7aa248caa3dc42b3570c2cdaff8
Dustin L. Dodson <dustinleedodson@gmail.com>
2017-10-06 20:19:12 -0800

    Added empty files so makefiles compile and fail.

---
+++ solutions/dldodson/contigsum.hpp
@@ -0,0 +1,11 @@
+#ifndef _CONTIGSUM_HPP_
+#define _CONTIGSUM_HPP_
+
+template <typename RAIter>
+const unsigned int contigSum(RAIter begin, RAIter end) {
+	unsigned int ret = 0;
+
+	return ret;
+}
+
+#endif
---
+++ solutions/dldodson/inversions.hpp
@@ -0,0 +1,11 @@
+#ifndef _INVERSIONS_HPP_
+#define _INVERSIONS_HPP_
+
+template <typename RAIter>
+const unsigned int inversions(RAIter begin, RAIter end) {
+	unsigned int ret = 0;
+
+	return ret;
+}
+
+#endif
