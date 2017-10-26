2cdd26e121c5e9bd2be213b0d7bc59c39b526b4e
Dustin L. Dodson <dustinleedodson@gmail.com>
2017-10-25 22:40:08 -0800

    Passing tests after much struggle.

--- solutions/dldodson/contigsum.hpp
+++ solutions/dldodson/contigsum.hpp
@@ -1,11 +1,22 @@
 #ifndef _CONTIGSUM_HPP_
 #define _CONTIGSUM_HPP_
 
-template <typename RAIter>
-const unsigned int contigSum(RAIter begin, RAIter end) {
-	unsigned int ret = 0;
+template<typename RAIter>
+const int contigSum(const RAIter & begin, const RAIter & end) {
+	int max = 0;
+	int tempMax = 0;
 
-	return ret;
+	for(auto i = begin; i < end; i++) {
+		tempMax += *i;
+
+		if(tempMax < 0)
+			tempMax = 0;
+
+		if(tempMax > max)
+			max = tempMax;
+	}
+	
+	return max;
 }
 
 #endif
--- solutions/dldodson/inversions.hpp
+++ solutions/dldodson/inversions.hpp
@@ -1,11 +1,64 @@
 #ifndef _INVERSIONS_HPP_
 #define _INVERSIONS_HPP_
 
-template <typename RAIter>
-const unsigned int inversions(RAIter begin, RAIter end) {
-	unsigned int ret = 0;
+/*
+ * This is a modified version of
+ * Dr. Chappell's Merge Sort algorithm.
+ */
 
-	return ret;
+#include <vector>
+using std::vector;
+
+template <typename RAIter>
+const size_t stableMerge(const RAIter & begin, const RAIter & middle, const RAIter & end) {
+	auto count = 0;
+	using Value = typename std::remove_reference<decltype(*begin)>::type;
+	
+	vector<Value> buffer(distance(begin, end));
+	
+	auto in1 = begin;
+	auto in2 = middle;
+	auto out = buffer.begin();
+	
+	while(in1 != middle && in2 != end) {
+		if(*in2 < *in1) {
+			*out++ = *in2++;
+			count += (middle - in1);
+		}
+		else
+			*out++ = *in1++;
+	}
+	
+	copy(in1, middle, out);
+	copy(in2, end, out);
+	
+	copy(buffer.begin(), buffer.end(), begin);
+	
+	return count;
+}
+
+template <typename RAIter>
+const size_t mergeSort(const RAIter & begin, const RAIter & end) {
+	auto count = 0;
+	
+	auto size = distance(begin, end);
+	
+	if(size <= 1)
+		return 0;
+	
+	auto middle = begin;
+	advance(middle, size/2);
+	
+	count += mergeSort(begin, middle);
+	count += mergeSort(middle, end);
+	
+	return stableMerge(begin, middle, end) + count;
+}
+
+
+template<typename RAIter>
+const size_t inversions(const RAIter & begin, const RAIter & end) {
+	return mergeSort(begin, end);
 }
 
 #endif
